0618ce472cd1d46e8fa9490156698429a2e657ac
“Deyvin <deyvinvelasquez0202@gmail.com>
2022-02-25 15:52:43 -0500

    Added a rating button

--- src/actions/restaurantDetails.js
+++ src/actions/restaurantDetails.js
@@ -1,16 +1,31 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { Rate } from "antd";
+import { Rate, Button } from "antd";
 
 export default function RestaurantDetails() {
-  const [restaurant, setRestaurant] = useState({});
+  const [restaurant, setRestaurant] = useState({}); 
+  const [rating, setRating] = useState();
   const params = useParams();
+
+  const handleRating = () => {
+    fetch(`https://bocacode-intranet-api.web.app/restaurants/${params.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ rating: rating }),
+    })
+    .then(response => response.json())
+    .then(() => setRating(0))
+    .catch(alert)
+  }
+
   useEffect(() => {
     fetch(`https://bocacode-intranet-api.web.app/restaurants/${params.id}`)
       .then((response) => response.json())
       .then((data) => setRestaurant(data))
       .catch(alert);
-  }, []);
+  }, [rating]);
 
   if (!restaurant.photoUrl) {
     return <p>Loading</p>;
@@ -18,11 +33,24 @@ export default function RestaurantDetails() {
 
   return (
     <section className="detail-wrapper">
-      <h1 style={{ marginTop: 80, fontSize: 42, fontWeight: 800 }}>
+      <img src={restaurant.photoUrl} alt={`IPhoto of ${restaurant.name}`} />
+      <div className="detail-restaurant-wrapper">
+      <h1 style={{ fontSize: 42, fontWeight: 800, marginBottom: 0 }}>
+        {" "}
         {restaurant.name}
       </h1>
       <Rate disabled defaultValue={restaurant.rating} />
-      <img src={restaurant.photoUrl} alt={`IPhoto of ${restaurant.name}`} />)
+      <span> ({restaurant.numRatings})</span>
+      <hr />
+      <h3> {restaurant.address}</h3>
+      <hr />
+      <h2 style={{ marginTop: "20px", fontSize: 28, fontWeight: 700 }}>
+        {" "}
+        Rate {restaurant.name}
+      </h2>
+      <Rate value={rating}onChange={(value) => setRating(value)} />
+      <Button onClick={handleRating} type="primary" size={"large"}>Submit Rating</Button>
+    </div>
     </section>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import "antd/dist/antd.css";
 import './index.css';
 import App from './App';
-import { BrowserRouter, Route } from 'react-router-dom';
 
 
 ReactDOM.render(
